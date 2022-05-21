bca5b649674ae48ae8e240e622d096a990c5b662
Yesho Simon <sepkap123@gmail.com>
2022-05-21 10:15:33 +0300

    add controller methods

--- app/Http/Controllers/TodoApiController.php
+++ app/Http/Controllers/TodoApiController.php
@@ -6,5 +6,30 @@ use Illuminate\Http\Request;
 
 class TodoApiController extends Controller
 {
-    //
-}
+    public function getAllTasks() {
+      
+      }
+    
+      public function createTask(Request $request) {
+        // logic to create a student record goes here
+      }
+  
+      public function getTask($id) {
+        // logic to get a student record goes here
+      }
+  
+      public function updateTask(Request $request, $id) {
+        // logic to update a student record goes here
+      }
+  
+      public function deleteTask ($id) {
+        // logic to delete a student record goes here
+      }
+    
+    
+    
+    
+    
+    
+    
+    }
